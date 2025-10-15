aebe2cf300c94177e06197bc9fce353a535b34b9
arashms1994 <arash.ms1994@gmail.com>
2025-10-15 11:29:07 +0330

    Feat: add call redirect for call section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Badges from "@/components/badges/Badges";
 import Cards from "@/components/cards/Cards";
 import CarouselHero from "@/components/carousel/Carousel";
 import CategoryGrid from "@/components/categories/Categories";
-import CallSection from "@/components/form/call-us/CallSection";
+import CallSection from "@/components/call-us/CallSection";
 import FormSection from "@/components/form/FormSection";
 
 export default function Home() {
