e3f4e7f597c3ce92c494d32061a55942c92c79ce
arashms1994 <arash.ms1994@gmail.com>
2025-12-02 15:43:25 +0330

    Fix

--- src/components/about-us/whay-us/WhyUs.tsx
+++ src/components/about-us/whay-us/WhyUs.tsx
@@ -1,7 +1,9 @@
 const WhyUs = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center p-4">
-      <h4 className="text-2xl font-semibold text-center">چرا ما را انتخاب کنید</h4>
+      <h4 className="text-2xl font-semibold text-center">
+        چرا ما را انتخاب کنید
+      </h4>
       <p className="text-center text-xl font-normal max-w-3xl mt-6">
         تمرکز بر صنعت نوارهای فولادی ضد زنگ تولید نوارهای فولادی ضد زنگ با
         کیفیت، از نوارهای فولادی ضد زنگ 304 با کیفیت بالا گرفته تا تولید و ارائه
