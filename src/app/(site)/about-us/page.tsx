a16f2d5e1656d110bf7e98027dbcc380c62cd902
arashms1994 <arash.ms1994@gmail.com>
2025-12-06 15:01:10 +0330

    Feat: ADD About us cards

--- src/app/(site)/about-us/page.tsx
+++ src/app/(site)/about-us/page.tsx
@@ -1,8 +1,10 @@
+import AboutUsCardsDiv from "@/components/about-us/cards/AboutUsCardsDiv";
 import WhyUs from "@/components/about-us/whay-us/WhyUs";
 
 const page = () => {
   return (
     <div>
+      <AboutUsCardsDiv />
       <WhyUs />
     </div>
   );
---
+++ src/components/about-us/cards/AboutUsCard.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export interface AboutUsCardProps {
+  image: string;
+  title: string;
+}
+
+const AboutUsCard = ({ image, title }: AboutUsCardProps) => {
+  return (
+    <div className="w-[300px] h-fit flex flex-col justify-center items-center gap-2">
+      <Image src={image} alt={title} width={300} height={380} />
+      <div className="w-[300px] h-16 flex justify-center items-center gap-2 rounded-2xl bg-button text-white text-center">
+        <h3 className="text-[28px] font-medium">{title}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsCard;
