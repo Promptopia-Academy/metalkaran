9a61192fdb75a7a6e8b0162420bca0eae3f12914
masoudjaliliaziz <masoudjalili0781@gmail.com>
2026-02-17 12:27:33 +0330

    debug import apis

--- src/app/(site)/products/[id]/page.tsx
+++ src/app/(site)/products/[id]/page.tsx
@@ -1,22 +1,15 @@
 import { IPageProps } from "@/types/type";
-import { notFound } from "next/navigation";
-import { getSiteProductById } from "@/lib/api";
+import { getProductById } from "@/lib/cms/producstsApi";
 import ProductDetail from "@/components/product/ProductDetail";
+import { IProduct } from "@/types/type";
+import { notFound } from "next/navigation";
 
 const ProductDetailPage = async ({ params }: IPageProps) => {
   const { id } = await params;
-  const productId = Number(id);
-
-  if (Number.isNaN(productId)) {
-    notFound();
-  }
-
-  const product = await getSiteProductById(productId);
-  console.log(product);
+  const product = (await getProductById(Number(id))) as IProduct;
   if (!product) {
     notFound();
   }
-
   return (
     <div className="px-4 sm:px-6 md:px-12 lg:px-24 xl:px-36 py-10 md:py-16">
       <ProductDetail product={product} />
