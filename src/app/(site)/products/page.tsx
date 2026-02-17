6d1c8f635dd725a761efbc5a8bb56aa68d039399
masoudjaliliaziz <masoudjalili0781@gmail.com>
2026-02-17 10:37:13 +0330

    debug products page

--- src/app/(site)/products/page.tsx
+++ src/app/(site)/products/page.tsx
@@ -1,4 +1,4 @@
-import { PRODUCT_ITEMS } from "@/lib/constants";
+"use client";
 import CardElement from "@/components/cards/CardElement";
 import { getSiteProducts } from "@/lib/api";
 import { IProduct } from "@/types/type";
@@ -9,10 +9,7 @@ const ProductsPage = async () => {
   return (
     <div className="w-full px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 py-8 md:py-12">
       <section className="w-full max-w-[1600px] mx-auto">
-        <h1
-          className="text-2xl md:text-3xl font-bold mb-6 md:mb-8"
-          dir="rtl"
-        >
+        <h1 className="text-2xl md:text-3xl font-bold mb-6 md:mb-8" dir="rtl">
           همه محصولات
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6">
@@ -20,7 +17,7 @@ const ProductsPage = async () => {
             <CardElement
               key={product.id}
               id={product.id}
-              image={product.image || "/carousel-img/image-1.png"}
+              image={product.image || ""}
               title={product.title}
               slug={product.standards || product.category?.title}
             />
