a6e34c473ce4302b2f5d933d4da65b050dab3ac2
masoudjaliliaziz <masoudjaliliaziz0781@gmail.com>
2026-06-07 13:27:17 +0330

    debug carosel

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -55,7 +55,7 @@ const CarouselHero = ({ heroSection: propHero }: CarouselHeroProps) => {
           <CarouselItem key={image.id} className="basis-5/7 lg:basis-8/10">
             <div className="group shrink-0 rounded-xl md:rounded-2xl overflow-hidden relative transition-all duration-500 ease-out h-[clamp(260px,55vh,420px)] md:h-[450px] lg:h-[480px] w-full">
               <Image
-                src={image.src}
+                src={`https://metalkarantech.ir:3001${image.src}`}
                 alt={image.alt}
                 fill
                 className="rounded-xl md:rounded-2xl object-cover transform transition-transform duration-700"
